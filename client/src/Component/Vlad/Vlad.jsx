6f4b05607a36e47ebb50d71d7e4d58ddcda666aa
VitaliyHr <grigorivv71@gmail.com>
2020-05-08 21:46:13 +0300

    Gitter

---
+++ client/src/Component/Vlad/Vlad.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import style from './Vlad.module.css'
+import { reduxForm, Field } from 'redux-form'
+import { createField } from '../Common/Field/Field'
+import { Input } from '../Common/FormInput/Input'
+import { require } from '../../Validators/Validate'
+
+
+const Vlad = React.memo(({ pizzas, ...props }) => {
+    console.log(pizzas)
+    return (
+        <div className={style.wrapper}>
+            {pizzas.map(pizza => <form key={pizza.id} onSubmit={props.handleSubmit} className={style.wrapperPizza}>
+                <div >
+                <img className={style.photo} src={pizza.photo} alt="NoImg" />
+                <div className={style.name}>{pizza.name}</div>
+                <div className={style.buy}>
+                    <div className={style.price}>{pizza.price} грн</div>
+                    <span className={style.count}>&minus;{createField('', `${pizza.id}`,'text', Input, [], style.countInput, null, (e) => props.onChange(e))}&#43;</span>
+                    <button key={pizza.id}  className={style.order}>Замовити</button>
+                </div>
+                </div>
+            </form>)}           
+        </div >
+    )
+})
+
+export const VladForm = reduxForm({ form: 'buy' })(Vlad)
+
+
