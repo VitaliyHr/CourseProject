e82d01eb46473f8edc3d7e3b12f1c4557c781a51
VitaliyHr <56275142+VitaliyHr@users.noreply.github.com>
2020-05-09 17:33:21 +0300

    Bug fixing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import config from 'config';
+import compression from 'compression';
+import helmet from 'helmet';
 import log4js from './middleware/log4js';
 import Pizza from './routes/pizza';
 
@@ -9,7 +11,11 @@ const logger = log4js.getLogger();
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('image'));
+app.use(helmet());
+app.disable('x-powered-by');
+app.use(compression());
+app.use('/image', express.static('image'));
+
 
 app.use('/pizza', Pizza);
 
