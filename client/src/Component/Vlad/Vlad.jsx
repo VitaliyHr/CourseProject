f46bd501e1b2acc17b89e203554223b8df450658
VitaliyHr <grigorivv71@gmail.com>
2020-05-09 14:39:19 +0300

    Redux-form and onSubmit

--- client/src/Component/Vlad/Vlad.jsx
+++ client/src/Component/Vlad/Vlad.jsx
@@ -6,8 +6,8 @@ import { Input } from '../Common/FormInput/Input'
 import { require } from '../../Validators/Validate'
 
 
-const Vlad = React.memo(({ pizzas, ...props }) => {
-    console.log(pizzas)
+const Vlad = React.memo(({count,setCount, pizzas, ...props }) => {
+    const { handleSubmit } = props;
     return (
         <div className={style.wrapper}>
             {pizzas.map(pizza => <form key={pizza.id} onSubmit={props.handleSubmit} className={style.wrapperPizza}>
@@ -17,11 +17,17 @@ const Vlad = React.memo(({ pizzas, ...props }) => {
                 <div className={style.buy}>
                     <div className={style.price}>{pizza.price} грн</div>
                     <span className={style.count}>
-                        <span className={style.symbol}>&minus;</span>
-                        {createField('', `${pizza.id}`,'text', Input, [], style.countInput, null, (e) => props.onChange(e))}
-                        <span className={style.symbol}>&#43;</span>
+                        <span className={style.symbol} onClick={()=>setCount(count-1)}>&minus;</span>
+                        {createField('', `count${pizza.name}`,'text', Input, [], style.countInput, count, (e) => props.onChange(e))}
+                        <span className={style.symbol} onClick={()=>{
+                            console.log(count)
+                            setCount(count+1)}}>&#43;</span>
                         </span>
-                    <button key={pizza.id}  className={style.order}>Замовити</button>
+                    <button key={pizza.id}  className={style.order} onClick={handleSubmit(values => 
+                            props.onSubmit({ 
+                            ...values,
+                            id:pizza.id
+                        }))}>Замовити</button>
                 </div>
                 </div>
             </form>)}           
--- client/src/Component/Vlad/VladContainer.js
+++ client/src/Component/Vlad/VladContainer.js
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { getPizzas } from '../../redux/Seloctors/pizzaCatalogSeloctors'
 import { compose } from 'redux'
 import { connect } from 'react-redux'
 import { VladForm } from './Vlad'
 
 const Vlad = React.memo((props)=>{
+    let [count,setCount] = useState(0)
     let onSubmit = (form)=>{
         debugger
     }
     let onChange = (e)=>{
-        debugger
+        if(e.target){
+            setCount(e.target.value)
+        }
+        
     }
     return(
-        <VladForm {...props} onSubmit={onSubmit} onChange={onChange}/>
+        <VladForm {...props} onSubmit={onSubmit} onChange={onChange} count={count} setCount={setCount}/>
     )
 })
 
